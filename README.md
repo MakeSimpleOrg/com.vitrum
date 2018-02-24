3a27f0fed3a43f42d4712f9e9a1ec1f05b7b0443
Dmitriy Ponomarev <diamond5170@gmail.com>
2018-02-24 23:29:01 +0300

    And BS wireless dimmers (I, II)

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ App for Vitrum switches.
 
 If you are using Homey version less than 1.5.6: after adding any Vitrum device, you must set all association groups to 1.1 manually.
 
-Support all EU wireless dimmers (I, II, III, IV, VI), EU wireless On-Off III, and BS wireless On-Off (I, II). But you can send me productTypeId and productId, if you want to add another devices.
+Support all EU wireless dimmers (I, II, III, IV, VI), EU wireless On-Off III, BS wireless dimmers (I, II), BS wireless On-Off (I, II).
+
+You can send me productTypeId and productId, if you want to add another devices.
---
+++ drivers/vitrum_2_sat_bs/device.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class Vitrum2SatBSDevice extends ZwaveDevice {
+	async onMeshInit() {
+		//this.enableDebug();
+
+		if(Object.keys(this.node.CommandClass).length < 5)
+		{
+			this.registerCapability('onoff', 'BASIC');
+			this.registerCapability('onoff', 'SWITCH_MULTILEVEL');
+			this.registerCapability('dim', 'BASIC');
+			this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+
+			this.registerReportListener('BASIC', 'BASIC_SET', ( rawReport, parsedReport ) => {
+				if(this.getCapabilityValue('onoff') == true && rawReport.Value == 0)
+					this.setCapabilityValue('onoff', false);
+				else if(this.getCapabilityValue('onoff') == false && rawReport.Value > 0)
+					this.setCapabilityValue('onoff', true);
+				this.setCapabilityValue('dim', rawReport.Value / 99);
+			});
+		}
+	}
+}
+
+module.exports = Vitrum2SatBSDevice;
